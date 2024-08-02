3fa26b6bb15b8dc12a013df12da7de7ee57b7768
CharlesMoseley <charlesmoseley33@outlook.com>
2024-08-01 23:16:37 -0400

    Update 8/1. Slider Prototype Complete

---
+++ GallonExtension/js/popup.js
@@ -0,0 +1,59 @@
+thing = document.getElementById("thingId");
+
+thing.innerHTML = ":3";
+thing.style.backgroundColor = "yellow";
+thing.style = "background-color:red;width:200px;height:100px;font-family:courier;" /* style is the attribute being edited and overriding the previous line and the style original defined in the html */
+thing.onmousedown = thingMouseDown;
+//thing.onmousemove = thingMouseMove;
+//thing.onmouseup = thingMouseUp;
+thing.onmouseover = highlight;
+thing.onmouseout = unhighlight;
+
+xPos = 100;
+yPos = 0;
+
+function myFunction(){
+	this.innerHTML = "Thanks";
+}
+
+function thingMouseDown(e){
+	this.onmousemove = nof;
+	this.innerHTML = updatePos(xPos);
+	this.onmousemove = thingMouseMove;
+	
+}
+
+function thingMouseMove(e){
+	this.onmousedown = nof;
+	xPos = e.pageX
+	this.innerHTML = updatePos(xPos);
+	this.onmouseup = thingMouseUp;
+}
+
+function thingMouseUp(){
+	this.onmousemove = nof;
+	this.innerHTML = updatePos(xPos);
+	this.onmousedown = thingMouseDown;
+}
+
+function nof(){
+	
+}
+
+function updatePos(pos){
+	if (pos < 50){
+		return 50;
+	} else if (pos > 100) {
+		return 100;
+	} else {
+		return pos;
+	}
+}
+
+function highlight(){
+	this.style.backgroundColor = "rgb(255,80,80)";
+}
+
+function unhighlight(){
+	this.style.backgroundColor = "red";
+}
